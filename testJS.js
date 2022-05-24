1cf6c4449292311d5a6fad9131f36c1d3c5edddb
Chris <chris.a.gonzalez97@gmail.com>
2022-05-23 19:25:38 -0700

    reorganized folders

--- testJS.js
+++
@@ -1,9 +0,0 @@
-const customId = 'confirmBuy_5555555_TSLA_50.83';
-const parsedId = customId.split('_');
-
-const money = 24.96;
-console.log(parseInt(money));
-
-/*for (let i = 0; i < parsedId.length; i++) {
-    console.log(parsedId[i])
-}*/
