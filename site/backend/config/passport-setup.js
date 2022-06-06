05cb46886cee94a79918534f5ccc22662f774e26
Chris <chris.a.gonzalez97@gmail.com>
2022-06-06 13:30:48 -0700

    added Dockerfiles

--- site/backend/config/passport-setup.js
+++ site/backend/config/passport-setup.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 var DiscordStrategy = require("passport-discord").Strategy;
-const { clientId, clientSecret } = require("../../../config.json");
+const { clientId, clientSecret } = require("../config.json");
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -11,7 +11,7 @@ passport.use(
         clientID: clientId,
         clientSecret: clientSecret,
         callbackURL: 'http://localhost:4000/auth/discord/callback',
-        scope: ["bot", "identify"]
+        scope: ["bot", "identify", "applications.commands"]
     },
     async (accessToken, refreshToken, profile, done) => {
         done(null, profile);
